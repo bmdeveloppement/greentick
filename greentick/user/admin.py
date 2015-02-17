00ac432d3c398413499fef65b155a4e71f96b2aa
Benoit Minard <benoit@Benoits-MacBook-Pro.local>
2015-02-17 22:20:48 +0100

    ADD - Models init

--- greentick/user/admin.py
+++ greentick/user/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from user.models import Company, User
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(User)
--- greentick/user/models.py
+++ greentick/user/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     subscription_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class User(models.Model):
     company = models.ForeignKey(Company)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100, unique=True)
     subscription_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     last_login_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)

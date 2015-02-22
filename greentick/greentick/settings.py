b02a797265692abaefbfc6dc681fce55a0a83cf5
Benoit Minard <benoit@Benoits-MacBook-Pro.local>
2015-02-22 01:30:10 +0100

    ADD - Better template & css management

--- greentick/greentick/settings.py
+++ greentick/greentick/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from os.path import dirname, join
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -87,6 +89,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Set global template & static folder
+TEMPLATE_DIRS = (
+    join(dirname(dirname(__file__)), 'templates'),
+)
+STATICFILES_DIRS = (
+    join(dirname(dirname(__file__)), 'static'),
+)
+
 
 # Uploaded files
 
